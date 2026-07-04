6f85a9d967ab438ef4546cc95f34526cd4192ba2
bigblok456 <kausik72005@gmail.com>
2026-07-05 00:08:34 +0530

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        //do the xor and the carry
+        while(b!=0){
+            int tmp=(a&b)<<1;
+            a=a^b;
+            b=tmp;
+        }
+        return a;
+
+        
+    }
+};
