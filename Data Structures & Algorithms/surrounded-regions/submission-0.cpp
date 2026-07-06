deac21370d814bf55a564d6094787e46f03ff041
bigblok456 <kausik72005@gmail.com>
2026-07-06 23:50:44 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board,int i,int j){
+        int m=board.size();
+        int n=board[0].size();
+        if(i<0 || i>=m || j>=n || j<0 ||(board[i][j]!='O')){
+            return;
+        }
+        board[i][j]='#';
+        //mark all the boundary Os and the Os connected to it by a hashtag
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j+1);
+        dfs(board,i,j-1);
+    }
+    void solve(vector<vector<char>>& board) {
+        int m=board.size();
+        int n=board[0].size();
+        //this is done so as to check the left and right boundary
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O'){
+                dfs(board,i,0);
+            }
+            if(board[i][n-1]=='O'){
+                dfs(board,i,n-1);
+            }
+        }
+        //this is done so as to check the top and the bottom boundary
+        for(int j=0;j<n;j++){
+            if(board[0][j]=='O'){
+                dfs(board,0,j);
+            }
+            if(board[m-1][j]=='O'){
+                dfs(board,m-1,j);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
+                }
+                else if(board[i][j]=='#'){
+                    board[i][j]='O';
+                }
+            }
+        }
+
+        
+    }
+};
